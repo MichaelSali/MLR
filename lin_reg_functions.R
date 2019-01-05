220c2bfaf7f5fe1feff67856ec4c593eccab56f8
MichaelSali <michael.selitrennik@gmail.com>
2019-01-05 17:23:03 +0200

    first commit

---
+++ lin_reg_functions.R
@@ -0,0 +1,19 @@
+compute.cost <- function(X,y,theta){
+  m <- length(y)
+  J <- 0
+  J <- sum((X%*%theta-y)^2)/(2*m)
+  return(J)
+}
+
+gradient.descent <- function(X,y,theta,alpha,num_iters){
+  m <- length(y)
+  J_history <- NULL
+  for (i in 1:num_iters) {
+    delta_theta <- c(0,0)
+    delta_theta[1] <- sum((X%*%theta-y)*X[,1])*alpha/m
+    delta_theta[2] <- sum((X%*%theta-y)*X[,2])*alpha/m
+    theta <- theta - delta_theta
+    J_history <- c(J_history,compute.cost(X, y, theta))
+  }
+  return(list('min.theta' = theta,'J.history' = J_history))
+}
